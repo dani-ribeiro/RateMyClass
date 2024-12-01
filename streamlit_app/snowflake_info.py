5287ac0f98a25c78d7256067a5407b34758a050e
dani-ribeiro <10DanielRibeiro@gmail.com>
2024-12-01 01:52:48 -0600

    .

--- streamlit_app/snowflake_info.py
+++
@@ -1,7 +0,0 @@
-class SnowflakeInfo:
-    # ANIMAL = 'TODO'
-    # USERNAME = 'TODO'
-    # PASSWORD = 'TODO'
-    # ACCOUNT = 'TODO'
-    # DATABASE = f'{ANIMAL}_DB'
-    # SCHEMA = f'RATE_MY_CLASS'
