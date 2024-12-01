b3a95ba360afb6e2ce2e16fc9255efe8ec28d271
Brian Wei <brian.b.wei@gmail.com>
2024-11-30 23:38:42 -0600

    streamlit web app

---
+++ streamlit_app/snowflake_info.py
@@ -0,0 +1,7 @@
+class SnowflakeInfo:
+    # ANIMAL = 'TODO'
+    # USERNAME = 'TODO'
+    # PASSWORD = 'TODO'
+    # ACCOUNT = 'TODO'
+    # DATABASE = f'{ANIMAL}_DB'
+    # SCHEMA = f'RATE_MY_CLASS'
